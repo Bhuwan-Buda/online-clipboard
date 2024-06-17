eafc1a96a38472e3c3e7e5507a649eeec931969d
Bhuwan Buda <bhuwanbuda143@gmail.com>
2024-06-17 08:19:31 +0545

    updated

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "message encrypter";
+const urlsToCache = [
+  "/",
+  "%PUBLIC_URL%/index.html",
+  // Add other resources you want to cache
+];
+
+document.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+document.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+document.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
